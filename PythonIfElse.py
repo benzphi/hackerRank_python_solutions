ae2e7fab510c356d99dd72d9a4c9e6bd67cc68c1
benz <51046001+benzphi@users.noreply.github.com>
2019-09-15 12:05:11 +0200

    Create PythonIfElse.py

---
+++ PythonIfElse.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2 != 0:
+        print("Weird")
+    elif n%2 == 0:
+        if n >= 6 and n <= 20:
+            print("Weird")
+        else:
+            print("Not Weird")
+    else:
+        pass
